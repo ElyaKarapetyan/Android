b7568e0bcf911ec078f4dc121ef7e84beea06aaf
Elya Karapetyan <elya.karapetyan95@mail.ru>
2018-07-22 22:46:35 +0400

    Change image task InProgress

---
+++ 1_22-07/TwoActivity/app/src/main/java/com/example/elya/twoactivty/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.elya.twoactivty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    CheckBox check;
+    Button firstButton;
+    Button secondButton;
+    EditText message;
+    Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.imgViewId);
+        check = (CheckBox) findViewById(R.id.checkboxId);
+        firstButton = (Button) findViewById(R.id.firstButtonId);
+        secondButton = (Button) findViewById(R.id.secondButtonId);
+        message =  (EditText) findViewById(R.id.messageId);
+        saveButton = (Button) findViewById(R.id.saveButtonId);
+
+    }
+}
