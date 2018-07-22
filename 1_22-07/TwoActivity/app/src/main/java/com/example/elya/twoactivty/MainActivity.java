f10de802658fbda3b7a5529fb7a5969fce99e0e0
Elya Karapetyan <elya.karapetyan95@mail.ru>
2018-07-23 00:01:32 +0400

    Completed task

--- 1_22-07/TwoActivity/app/src/main/java/com/example/elya/twoactivty/MainActivity.java
+++ 1_22-07/TwoActivity/app/src/main/java/com/example/elya/twoactivty/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.elya.twoactivty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,25 +10,34 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView img;
-    CheckBox check;
-    Button firstButton;
-    Button secondButton;
-    EditText message;
-    Button saveButton;
+    private ImageView img;
+    private CheckBox check;
+    private Button firstButton;
+    private Button secondButton;
+    private EditText message;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img = (ImageView) findViewById(R.id.imgViewId);
-        check = (CheckBox) findViewById(R.id.checkboxId);
-        firstButton = (Button) findViewById(R.id.firstButtonId);
-        secondButton = (Button) findViewById(R.id.secondButtonId);
-        message =  (EditText) findViewById(R.id.messageId);
-        saveButton = (Button) findViewById(R.id.saveButtonId);
+        img = findViewById(R.id.imgViewId);
+        check = findViewById(R.id.checkboxId);
+        firstButton = findViewById(R.id.firstButtonId);
+        secondButton = findViewById(R.id.secondButtonId);
+        saveButton = findViewById(R.id.saveButtonId);
 
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (check.isChecked()) {
+                    img.setVisibility(View.VISIBLE);
+                } else {
+                    img.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
         firstButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,5 +50,19 @@ public class MainActivity extends AppCompatActivity {
                 img.setImageResource(R.drawable.second);
             }
         });
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage(view);
+            }
+        });
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        message = findViewById(R.id.messageId);
+        String messageTxt = message.getText().toString();
+        intent.putExtra("com.example.elya.twoactivty",messageTxt);
+        startActivity(intent);
     }
 }
