70df7d925f0a3df01d657e9102e4256316779202
Elya Karapetyan <elya.karapetyan95@mail.ru>
2018-07-31 22:43:18 +0400

    in progress

---
+++ 7_31-07/SearchUser/app/src/main/java/com/example/student/searchuser/ScrollingActivity.java
@@ -0,0 +1,39 @@
+package com.example.student.searchuser;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        String name = getIntent().getStringExtra(String.valueOf(MyAdapter.NAME_KEY));
+        toolbar.setTitle(name);
+        setSupportActionBar(toolbar);
+
+        ImageView imageView = findViewById(R.id.img);
+        String url = getIntent().getStringExtra(String.valueOf(MyAdapter.URL_KEY));
+        Picasso.get().load(url).into(imageView);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
